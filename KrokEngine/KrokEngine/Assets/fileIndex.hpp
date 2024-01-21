3b19474135282be261d54fadf9235d8cc5898db4
Krokoman13 <bramtehaken@gmail.com>
2024-01-22 00:37:11 +0100

    Reloaded resc files

--- KrokEngine/KrokEngine/Assets/fileIndex.hpp
+++ KrokEngine/KrokEngine/Assets/fileIndex.hpp
@@ -8,15 +8,13 @@
 #define RS__HOLOGRAM_FRAG	1		 //Assets\shaders\hologram.frag
 #define RS__HOLOGRAM_VERT	2		 //Assets\shaders\hologram.vert
 #define RS__TEXTURE_FRAG	3		 //Assets\shaders\texture.frag
-#define RS__BLUE_BALL_PNG	12		 //Assets\textures\Debug\Blue Ball.png
 #define RS__TEXTURE_VERT	4		 //Assets\shaders\texture.vert
 #define RS__BRICKS_JPG	5		 //Assets\textures\bricks.jpg
-#define RS__BALL_PNG	14		 //Assets\textures\Debug\Ball.png
-#define RS__PIXEL_PNG	15		 //Assets\textures\Debug\Pixel.png
-#define RS__RED_BALL_PNG	13		 //Assets\textures\Debug\Red Ball.png
-#define RS__LAND_JPG	6		 //Assets\textures\land.jpg
-#define RS__POOLTABLEFRAME_PNG	7		 //Assets\textures\PoolTableFrame.png
-#define RS__RUNICFLOOR_PNG	8		 //Assets\textures\runicfloor.png
-#define RS__SMILEY_JPG	9		 //Assets\textures\Smiley.jpg
-#define RS__VERYBIG_JPG	10		 //Assets\textures\veryBig.jpg
-#define RS__WINDOWSIMAGE_JPG	11		 //Assets\textures\windowsImage.jpg
+#define RS__BALL_PNG	6		 //Assets\textures\Debug\Ball.png
+#define RS__PIXEL_PNG	7		 //Assets\textures\Debug\Pixel.png
+#define RS__LAND_JPG	8		 //Assets\textures\land.jpg
+#define RS__POOLTABLEFRAME_PNG	9		 //Assets\textures\PoolTableFrame.png
+#define RS__RUNICFLOOR_PNG	10		 //Assets\textures\runicfloor.png
+#define RS__SMILEY_JPG	11		 //Assets\textures\Smiley.jpg
+#define RS__VERYBIG_JPG	12		 //Assets\textures\veryBig.jpg
+#define RS__WINDOWSIMAGE_JPG	13		 //Assets\textures\windowsImage.jpg
